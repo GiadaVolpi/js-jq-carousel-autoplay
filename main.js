9542b86cdf470e14f7a87da3fc3632429450ba3c
Gi@da <giadavolpi8@gmail.com>
2019-11-28 15:59:52 +0100

    carousel autoplay OK

---
+++ main.js
@@ -0,0 +1,96 @@
+// eseguo il file Javascript quando HTML è caricato
+$ (document).ready (function () {
+
+
+    $ (".next").click (function () {
+        // prendo l'immagine attualmente visibile
+        var imgAttuale = $ ("#slide img.active");
+
+        // prendo l'immagine successiva a quella attualmente visibile
+        var imgSuccessiva = imgAttuale.next ("img");
+
+        // prendo il cerchio attivo (deve avere il pieno visibile e il vuoto invisibile)
+        var dotActive = $ (".circle.circle-active");
+
+        // prendo il cerchio successivo a quello attivo
+        var dotSuccessivo = dotActive.next ();
+
+        // rendo invisibile la foto attuole
+        imgAttuale.removeClass ("active");
+
+        // disattivo il cerchio attivo
+        dotActive.removeClass ("circle-active");
+
+        // se l'immagine successiva non esiste
+        if (imgSuccessiva.length == 0) {
+            // rendo nuovamente visibile la prima del ciclo
+            imgSuccessiva = $ (".restart");
+
+            // rendo nuovamente visibile il primo dot
+            dotSuccessivo = $ (".circle-first")
+        }
+
+        // rendo visibile la foto successiva
+        imgSuccessiva.addClass ("active");
+
+        // rendo attivo il dot successivo
+        dotSuccessivo.addClass ("circle-active");
+    });
+
+
+    $ (".prev").click (function () {
+        // prendo l'immagine attualmente visibile
+        var imgAttuale = $ ("#slide img.active");
+
+        // prendo l'immagine precedente a quella attuale
+        var imgPrecedente = imgAttuale.prev ("img");
+
+        // prendo il cerchio attivo (deve avere il pieno visibile e il vuoto invisibile)
+        var dotActive = $ (".circle.circle-active");
+
+        // prendo il cerchio precedente a quello attivo
+        var dotPrecedente = dotActive.prev ();
+
+        // rendo invisibile la foto attuole
+        imgAttuale.removeClass ("active");
+
+        // disattivo il cerchio attivo
+        dotActive.removeClass ("circle-active");
+
+        // se l'immagine precedente non esiste
+        if (imgPrecedente.length == 0) {
+            // rendo nuovamente visibile l'ultima del ciclo
+            imgPrecedente = $ (".last");
+
+            // rendo nuovamente visibile l'ultimo dot
+            dotPrecedente = $ (".circle-last")
+        }
+
+        // rendo visibile la foto precedente
+        imgPrecedente.addClass ("active");
+
+        // rendo attivo il dot precedente
+        dotPrecedente.addClass ("circle-active");
+    });
+
+
+    // inserisco nella funzione AUTOPLAY tutti gli ordini che ho dato a NEXT
+    var autoplay = setInterval (function () {
+        var imgAttuale = $ ("#slide img.active");
+        var imgSuccessiva = imgAttuale.next ("img");
+        var dotActive = $ (".circle.circle-active");
+        var dotSuccessivo = dotActive.next ();
+        imgAttuale.removeClass ("active");
+        dotActive.removeClass ("circle-active");
+        if (imgSuccessiva.length == 0) {
+            imgSuccessiva = $ (".restart");
+            dotSuccessivo = $ (".circle-first")
+            // mostrate tutte le immagini una volta, torno alla prima e interrompo l'autoplay
+            clearInterval (autoplay);
+        }
+        imgSuccessiva.addClass ("active");
+        dotSuccessivo.addClass ("circle-active");
+    }, 3000);
+
+
+});
